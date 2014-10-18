de3f5bb430e94a308da45dc25588669d55ff75ca
Agis- <corestudiosinc@gmail.com>
2014-10-19 01:43:56 +0300

    Properly return status codes in case of errors

---
+++ benchmarks.sh
@@ -0,0 +1,1 @@
+ab -p 'bench.jpg' -T 'multipart/form-data' -H 'Image-Height: 100' -n 1000 -c4 http://localhost:12345/
--- main.go
+++ main.go
@@ -1,29 +1,66 @@
 package main
 
 import(
-  "image"
   "net/http"
-  "fmt"
-  "github.com/nfnt/resize"
+  "image"
   "image/jpeg"
+  _ "image/png"
+  _ "image/gif"
+  "github.com/nfnt/resize"
+  "strconv"
+  "log"
 )
 
+// Scaler is a service that scales images and ....
 func main() {
-  http.HandleFunc("/", foo)
+  http.HandleFunc("/", scaleImage)
 
   err := http.ListenAndServe(":12345", nil)
   if err != nil {
-    panic(err)
+    log.Fatal(err)
   }
 }
 
-func foo(w http.ResponseWriter, r *http.Request) {
+// scaleImage reads an image from r's request body and writes a resized version
+// to w's response body.
+//
+// The desired dimensions are denoted by r's "Image-Width" & "Image-Height"
+// headers.
+func scaleImage(w http.ResponseWriter, r *http.Request) {
+  var width, height uint64
+
   img, _, err := image.Decode(r.Body)
   if err != nil {
-    fmt.Println(err)
+    w.WriteHeader(http.StatusBadRequest)
+    w.Write([]byte(err.Error()))
+    return
   }
 
-  img = resize.Resize(300, 300, img, resize.NearestNeighbor)
+  if r.Header["Image-Width"] == nil && r.Header["Image-Height"] == nil {
+    w.WriteHeader(http.StatusBadRequest)
+    w.Write([]byte("Both Image-Width & Image-Height headers are missing"))
+    return
+  }
+
+  if r.Header["Image-Width"] != nil {
+    width, err = strconv.ParseUint(r.Header["Image-Width"][0], 10, 16)
+    if err != nil {
+      w.WriteHeader(http.StatusBadRequest)
+      w.Write([]byte("Malformed Image-Width header"))
+      return
+    }
+  }
+
+  if r.Header["Image-Height"] != nil {
+    height, err = strconv.ParseUint(r.Header["Image-Height"][0], 10, 16)
+    if err != nil {
+      w.WriteHeader(http.StatusBadRequest)
+      w.Write([]byte("Malformed Image-Height header"))
+      return
+    }
+  }
+
+  img = resize.Resize(uint(width), uint(height), img, resize.NearestNeighbor)
 
   err = jpeg.Encode(w, img, nil)
   if err != nil {
